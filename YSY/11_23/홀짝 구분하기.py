902ee50e895d992352869f2f1c5b88693a350291
SoyounG13 <2870116@naver.com>
2023-11-24 01:20:43 +0900

    11/23

---
+++ YSY/11_23/홀짝 구분하기.py
@@ -0,0 +1,6 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    answer = my_string[0:s]+overwrite_string+my_string[s+len(overwrite_string):]
+    print(answer)
+    
+    return answer

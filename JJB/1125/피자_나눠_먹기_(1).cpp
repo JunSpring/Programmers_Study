ff16552f18b403827aa2a1fd62021bb019d4b2f9
JunSpring <wjdwnsqja08@gmail.com>
2023-11-25 17:47:30 +0900

    11/25

---
+++ JJB/1125/피자_나눠_먹기_(1).cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 0;
+    
+    answer = ceil(n / 7.0);
+    
+    return answer;
+}
